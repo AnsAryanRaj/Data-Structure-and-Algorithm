64eb78b2ed707852177ae43e84ba462b65d75fcd
Aryan Raj <ansaryanpratap14@gmail.com>
2026-07-12 00:41:19 +0530

    Edition

---
+++ Strings/toggleTheCaseOfLetters.cpp
@@ -0,0 +1,19 @@
+// #include<iostream>
+// #include<string>
+// #include<cctype>
+// using namespace std;
+// int main() {
+//     string s="AlphaManIam";
+//     int n=s.length();
+//     for (char ch: s) {
+//         if (islower(ch)) {
+//            ch=toupper(ch);
+//         }
+//         else if (isupper(ch)) {
+//             ch=tolower(ch);
+//         }
+//         cout<<ch;
+//     }
+//
+//     return 0;
+// }
