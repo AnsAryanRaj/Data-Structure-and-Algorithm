d945f1bc62edccdfd04e4b1486d2ce57288fab86
Aryan Raj <ansaryanpratap14@gmail.com>
2026-07-12 17:51:52 +0530

    Edition

---
+++ Strings/CheckIfaStringIsAPalindromeIgnoringCase.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// #include<string>
+// #include<cctype>
+// #include<algorithm>
+// using namespace std;
+// int main() {
+//     string s="Madam";
+//     int n=s.length();
+//     int left=0;
+//     int right=s.length()-1;
+//
+//     while (left< right) {
+//         if (tolower(s[left]) != tolower(s[right])) {
+//             cout<<"Not Palindrome";
+//             return 0;
+//         }
+//         left++;
+//         right--;
+//
+//
+//     }
+//
+//     cout<<"Palindrome";
+//
+//
+// }
