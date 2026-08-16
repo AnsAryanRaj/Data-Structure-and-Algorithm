eb348b7231f680647e00e528c75cf10791149a12
Aryan Raj <ansaryanpratap14@gmail.com>
2026-08-16 16:54:29 +0530

    Edition

---
+++ Basics/SavingTaxes.cpp
@@ -0,0 +1,32 @@
+// // #include<iostream>
+// // using namespace std;
+// //
+// // int main() {
+// //
+// //     int T;
+// //     cin>>T;
+// //     int X, Y;
+// //
+// //     while(T>0){
+// //         cin>> X>> Y;
+// //         cout<<X-Y<<endl;
+// //         T--;
+// //     }
+// //     return 0;
+// //
+// // }
+//
+//
+// //Method 2-
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int T;
+//     cin>>T;
+//     int X, Y;
+//     for (int i=0;i<T;i++) {
+//         cin>>X>>Y;
+//         cout<<X-Y<<endl;
+//     }
+//     return 0;
+// }
---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,39 @@
+// #include<iostream>
+// using namespace std;
+//
+// void bubbleSort(int arr[], int n) {
+//     for (int i=0;i<n-1;i++) {
+//         bool isSwap=false;  // hum mante hai ki array hai sorted nhi hai
+//         for (int j=0;j<n-i-1;j++) {
+//             if (arr[j]>arr[j+1]) {
+//                 swap(arr[j], arr[j+1]);
+//
+//                 isSwap=true;
+//             }
+//         }
+//         if (!isSwap) { // array is already sorted
+//             //"Koi swapping nahi hui, matlab array already sorted hai.
+//             //Aur passes karne ki zarurat nahi."
+//
+//             return;
+//         }
+//     }
+// }
+//
+// void printArray(int arr[], int n) {
+//
+//     for (int i=0;i<n;i++) {
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+// int main() {
+//     int n=5;
+//     int arr[]{4,1,5,2,3};
+//
+//     bubbleSort(arr, n);
+//     printArray(arr,n);
+//     return 0;
+//
+//
+// }
